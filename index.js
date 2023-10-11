131a7e2e687dcb87003f7c82025302a763993c2d
Abdullahi Bashir Wale <bashabdulbash@gmail.com>
2023-10-11 10:20:23 +0200

    changes

--- index.js
+++ index.js
@@ -3,26 +3,31 @@ const { initReactI18next } = require("react-i18next");
 const backend = require("i18next-http-backend");
 const LanguageDetector = require("i18next-browser-languagedetector");
 
-const i18n = i18next.createInstance();
+/* const i18n = i18next.createInstance(); */
 
-i18n
-  .use(backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    backend: {
-      /*   loadPath: "/src/locales/{{lng}}/{{ns}}.json", */
-    },
-    lng: "en",
-    fallbackLng: "en",
-    ns: ["common", "otherNamespace"],
-    defaultNS: "common",
-    interpolation: {
-      escapeValue: false,
-    },
-    debug: true,
-  });
-// Export the i18n object using module.exports
-module.exports = i18n;
+/* module.exports = i18n; */
 
-console.log("Load Path:", i18n.options.backend.loadPath);
+(async function test() {
+  const i18n = await i18next
+    .use(backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+      backend: {
+        loadPath: "/src/locales/{{lng}}/{{ns}}.json",
+      },
+      lng: "en",
+      fallbackLng: "en",
+      ns: ["common", "otherNamespace"],
+      defaultNS: "common",
+      interpolation: {
+        escapeValue: false,
+      },
+      debug: true,
+    });
+
+  const translatedText = i18n.t("welcome");
+  console.log("Translated Text:", translatedText);
+  console.log("Load Path:", i18n.options.backend.loadPath);
+  console.log("Language Changed to French");
+})();
